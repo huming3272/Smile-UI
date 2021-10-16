e643a818069cf83e3f092ed9c6f2c1c7dd71d63b
胡鸣 <9185847+huming3272@user.noreply.gitee.com>
2021-10-16 20:34:19 +0800

    topnav样式更新

--- .history/src/components/Home_20211016203146.vue
+++ .history/src/components/Home_20211016203146.vue
@@ -18,22 +18,8 @@
   
 </template>
 <style lang="scss" scoped>
-  .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  .topnar{
+      border: 1px solid red;
     }
-  }
-}
   
 </style> 
