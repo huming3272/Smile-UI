741949af8cfdbee5c7cdb3dcf4ea158c49e13301
胡鸣 <9185847+huming3272@user.noreply.gitee.com>
2022-03-27 22:09:02 +0800

    修改 icon显示，部分样式和体验

---
+++ .history/src/views/Home_20220320154433.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="topnavAndBanner">
+    <Topnav />
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
+    <div class="features">
+          <svg class="icon">
+              <use xlink:href="#icon-Smile"></use>
+            </svg>
+            <svg class="icon">
+              <use xlink:href="#icon-vue"></use>
+            </svg>
+            <svg class="icon">
+              <use xlink:href="#icon-ts"></use>
+            </svg>
+      </div>
+  </div>
+</template>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
+</script>
+<style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner{
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
+  // 实现页面曲
+}
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: $green;
+      display: inline-block;
+      $h: 28px;
+      border-radius: $h/2;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+    }
+    &:hover{
+      text-decoration: none;
+    }
+  }
+}
+.features {
+    >svg {
+      width: 64px;
+      height: 64px; 
+    }
+}
+
+</style> 
---
+++ .history/src/views/Home_20220320155751.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="topnavAndBanner">
+    <Topnav />
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
+    <div class="features">
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-linggan"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
+</script>
+<style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+  clip-path: ellipse(80% 60% at 50% 40%);
+  // 实现弯曲页面部分
+}
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: $green;
+      display: inline-block;
+      $h: 28px;
+      border-radius: $h/2;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+    }
+    &:hover {
+      text-decoration: none;
+    }
+  }
+}
+.features {
+  margin: 64px auto
+  ul>li> svg {
+    width: 64px;
+    height: 64px;
+  }
+}
+</style> 
